8d2ce2af36e47edcc76ce77781884b08d7f10168
Fernando Quian Nogueira <fernandoquiannogueira@Fernandos-Mac-mini.local>
2023-11-26 20:21:07 -0300

    image light

--- script.js
+++ script.js
@@ -7,9 +7,6 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     img.setAttribute("src", "./assets/fernando.jpeg")
   } else {
-    img.setAttribute(
-      "src",
-      "https://media.licdn.com/dms/image/C4E03AQEzArxl80sYrg/profile-displayphoto-shrink_800_800/0/1636688205031?e=1700697600&v=beta&t=bWhJY4tDxWLb7krJYGK4tUop5VMsxe5-sI4pWyi8CU8"
-    )
+    img.setAttribute("src", "./assets/fernando dark.png")
   }
 }
